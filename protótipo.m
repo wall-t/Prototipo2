96dc1987a3d5baac69ce683733951cd931a19caa
Walter Santos <walter.sentinela@outlook.com>
2016-09-06 23:33:17 -0300

    Matlab
    testando...

---
+++ protótipo.m
@@ -0,0 +1,21 @@
+A = 5;
+B = 3;
+if A==B
+    disp ('A=B')
+elseif A>B
+    disp ('A maior que B')
+else
+    disp ('A menor que B')
+end
+
+
+for i = 1:1:7
+    C = i^2 + 1;
+end
+C
+
+while C < 50
+    B = B + 1;
+    
+end
+B
